2fda56082b706ef25edf5d3b361ef3504ebfb9bc
Norsang <thenorsang@gmail.com>
2023-11-21 15:14:28 -0500

    game logic done. Need to keep highscore now.

--- app/src/main/java/com/cs388/humanbenchmark/ReflexGameActivity.kt
+++ app/src/main/java/com/cs388/humanbenchmark/ReflexGameActivity.kt
@@ -1,10 +1,10 @@
 package com.cs388.humanbenchmark
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.graphics.Color
 import android.os.Bundle
 import android.os.CountDownTimer
-import android.util.Log
-import android.view.View
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.cs388.humanbenchmark.R
@@ -12,15 +12,23 @@ import com.cs388.humanbenchmark.R
 class ReflexGameActivity : AppCompatActivity() {
 
     private lateinit var textView: TextView
+    private lateinit var highScoreTextView: TextView
+    private lateinit var sharedPreferences: SharedPreferences
     private var startTime: Long = 0
     private var isGameRunning: Boolean = false
     private var isGameEnded: Boolean = false
+    private val HIGH_SCORE_KEY = "high_score"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_reflex_game)
 
         textView = findViewById(R.id.tapToStart)
+        highScoreTextView = findViewById(R.id.highScoreTextView)
+        textView.setTextColor(Color.BLACK)
+        highScoreTextView.setTextColor(Color.BLACK)
+        sharedPreferences = getPreferences(Context.MODE_PRIVATE)
+
         textView.setOnClickListener {
             if (!isGameRunning && !isGameEnded) {
                 startGame()
@@ -36,8 +44,8 @@ class ReflexGameActivity : AppCompatActivity() {
         // Change text and background color
         textView.text = getString(R.string.tap_when_red)
         textView.setBackgroundColor(Color.RED)
+        textView.setTextColor(Color.WHITE)
         isGameRunning = true
-        isGameEnded = false
 
         // Set a timer to turn the screen green after a random time
         object : CountDownTimer((1000 + Math.random() * 3000).toLong(), 100) {
@@ -54,6 +62,7 @@ class ReflexGameActivity : AppCompatActivity() {
     private fun turnScreenGreen() {
         textView.text = getString(R.string.tap_when_green)
         textView.setBackgroundColor(Color.GREEN)
+        textView.setTextColor(Color.WHITE)
         startTime = System.currentTimeMillis()
 
         textView.setOnClickListener {
@@ -70,13 +79,25 @@ class ReflexGameActivity : AppCompatActivity() {
         if (isGameRunning && textView.text == getString(R.string.tap_when_green)) {
             // Record the reaction time
             val reactionTime = System.currentTimeMillis() - startTime
+
+            // Retrieve the current high score
+            val currentHighScore = sharedPreferences.getLong(HIGH_SCORE_KEY, Long.MAX_VALUE)
+
+            // Check if the current reaction time is lower than the high score
+            if (reactionTime < currentHighScore) {
+                // Update the high score
+                sharedPreferences.edit().putLong(HIGH_SCORE_KEY, reactionTime).apply()
+                // Update the high score text view
+                highScoreTextView.text = getString(R.string.high_score, reactionTime)
+            }
+
             // Display the reaction time
             textView.text = getString(R.string.reaction_time, reactionTime)
             isGameEnded = true
 
             textView.postDelayed({
                 resetGame()
-            }, 1000)
+            }, 3000)
         }
     }
 
@@ -94,6 +115,7 @@ class ReflexGameActivity : AppCompatActivity() {
     private fun resetGame() {
         textView.text = getString(R.string.tap_to_start)
         textView.setBackgroundColor(Color.WHITE)
+        textView.setTextColor(Color.BLACK)
         isGameRunning = false
         isGameEnded = false // Reset the game-ended flag
 
@@ -108,5 +130,4 @@ class ReflexGameActivity : AppCompatActivity() {
             }
         }
     }
-
 }
