8e52d373a551945be3e24e4ff901f5790422c1c3
Christopher Mejia <58187990+chrism1001@users.noreply.github.com>
2023-12-08 16:53:40 -0500

    clicking on the target now sets new x and y coordinates

--- app/src/main/java/com/cs388/humanbenchmark/AimTrainerGameActivity.kt
+++ app/src/main/java/com/cs388/humanbenchmark/AimTrainerGameActivity.kt
@@ -9,22 +9,24 @@ import android.widget.FrameLayout
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.FrameLayout.LayoutParams
-import java.lang.Math.floor
 
 class AimTrainerGameActivity : AppCompatActivity() {
-    var positions = mutableListOf<Pair<Int, Int>>()
-    var gameTargets: Int = 30
-    var gamePlaying: Boolean = false
-    var layoutParams = LayoutParams(
+    private var positions = mutableListOf<Pair<Int, Int>>()
+    private var gameTargets: Int = 30
+    private var gamePlaying: Boolean = false
+    private var startTime: Long = 0
+    private var averageTime: Long = 0
+    private var layoutParams = LayoutParams(
         LayoutParams.WRAP_CONTENT,
         LayoutParams.WRAP_CONTENT
     )
 
-    lateinit var startGameButton: Button
-    lateinit var titleText: TextView
-    lateinit var scoreText: TextView
-    lateinit var gameContent: FrameLayout
-    lateinit var targetView: ImageView
+    private lateinit var startGameButton: Button
+    private lateinit var titleText: TextView
+    private lateinit var targetText: TextView
+    private lateinit var scoreText: TextView
+    private lateinit var gameContent: FrameLayout
+    private lateinit var targetView: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,6 +34,7 @@ class AimTrainerGameActivity : AppCompatActivity() {
 
         startGameButton = findViewById(R.id.aim_start_game_button)
         titleText = findViewById(R.id.aim_trainer_game_title)
+        targetText = findViewById(R.id.aim_trainer_target_text)
         scoreText = findViewById(R.id.aim_trainer_score_text)
         gameContent = findViewById(R.id.aim_trainer_game_content)
         targetView = findViewById(R.id.target)
@@ -45,21 +48,23 @@ class AimTrainerGameActivity : AppCompatActivity() {
         }
     }
 
-    fun startGame() {
+    private fun startGame() {
         gamePlaying = true
-        startGameButton.visibility = View.INVISIBLE
-        titleText.visibility = View.INVISIBLE
-        scoreText.visibility = View.VISIBLE
+        startGameButton.visibility = View.GONE
+        titleText.visibility = View.GONE
+        targetText.visibility = View.VISIBLE
         gameContent.visibility = View.VISIBLE
         Log.d("positions", "positions created = $positions")
         newTarget()
     }
 
-    fun newTarget() {
-        layoutParams.setMargins(floor(Math.random() * 800).toInt(), floor(Math.random() * 1300).toInt(), 0, 0)
-        targetView.setLayoutParams(layoutParams)
+    private fun newTarget() {
+        layoutParams.setMargins(kotlin.math.floor(Math.random() * 800).toInt(), kotlin.math.floor(
+            Math.random() * 1300
+        ).toInt(), 0, 0)
+        targetView.layoutParams = layoutParams
     }
-    fun targetHit(id: Int) {
+    private fun targetHit(id: Int) {
         if (!gamePlaying)
             return
         if (gameTargets == 0)
@@ -67,13 +72,26 @@ class AimTrainerGameActivity : AppCompatActivity() {
 
         Log.d("target id", "target id = $id")
         if (gameTargets != 0) {
+            Log.d("time elapsed", "${System.currentTimeMillis() - startTime}")
+            averageTime += System.currentTimeMillis() - startTime
             gameTargets--
-            scoreText.text = "Targets: ${gameTargets}"
+            targetText.text = "Targets: $gameTargets"
             newTarget()
         }
     }
 
-    fun gameOver() {
-
+    private fun gameOver() {
+        Log.d("score", "Your score! ${averageTime/30}")
+        scoreText.text = "Average Time: ${averageTime/30}"
+        gameTargets = 30
+        averageTime = 0
+        gamePlaying = false
+        gameContent.removeAllViews()
+        targetText.text = "Targets: 30"
+        targetText.visibility = View.GONE
+        gameContent.visibility = View.GONE
+        titleText.visibility = View.VISIBLE
+        scoreText.visibility = View.VISIBLE
+        startGameButton.visibility = View.VISIBLE
     }
 }
